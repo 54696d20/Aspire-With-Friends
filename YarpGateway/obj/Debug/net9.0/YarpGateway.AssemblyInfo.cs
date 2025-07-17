157998af49b751785d39a0406af5c16bc7e49642
Tim <35900408+54696d20@users.noreply.github.com>
2025-07-17 13:13:20 -0400

    Updated to use the mud theme

--- YarpGateway/obj/Debug/net9.0/YarpGateway.AssemblyInfo.cs
+++ YarpGateway/obj/Debug/net9.0/YarpGateway.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YarpGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96eb4ae953d8aacf7ca252ad2e2d74fa93f5e1af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3efec568abb173e2da0dc2d0b1c4e548a3b2bf0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("YarpGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YarpGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
