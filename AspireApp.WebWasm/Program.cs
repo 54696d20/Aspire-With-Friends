96eb4ae953d8aacf7ca252ad2e2d74fa93f5e1af
Tim <35900408+54696d20@users.noreply.github.com>
2025-07-17 11:47:26 -0400

    added mudblazor and a test button for snackbar

--- AspireApp.WebWasm/Program.cs
+++ AspireApp.WebWasm/Program.cs
@@ -2,8 +2,11 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using AspireApp.WebWasm;
 using AspireApp.WebWasm.Services;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Services.AddMudServices();
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
